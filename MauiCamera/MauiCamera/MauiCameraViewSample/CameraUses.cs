314891bb36577d306e83c81703598a7cf887a189
murilo martins <diasmmurilo@gmail.com>
2025-04-29 13:36:22 -0300

    Funconando camera com classes

---
+++ MauiCamera/MauiCamera/MauiCameraViewSample/CameraUses.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Core.Primitives;
+
+namespace MauiCameraViewSample
+{
+    public class CameraUses
+    {
+        private readonly ICameraProvider cameraProvider;
+        private readonly CameraView MyCamera;
+        private readonly Image MyImage;
+
+        public CameraUses(ICameraProvider cameraProvider, CameraView cameraView, Image image)
+        {
+            this.cameraProvider = cameraProvider;
+            this.MyCamera = cameraView;
+            this.MyImage = image;
+
+            MyCamera.MediaCaptured += MyCamera_MediaCaptured;
+        }
+
+        public async Task InitializeCameraAsync()
+        {
+            await cameraProvider.RefreshAvailableCameras(CancellationToken.None);
+            // Aqui substituímos o CameraPosition.Rear por uma busca pela câmera traseira
+            MyCamera.SelectedCamera = cameraProvider.AvailableCameras
+                .Where(c => c.Position == CameraPosition.Rear).FirstOrDefault();
+        }
+
+        public void Dispose()
+        {
+            MyCamera.MediaCaptured -= MyCamera_MediaCaptured;
+            MyCamera.Handler?.DisconnectHandler();
+        }
+
+        private void MyCamera_MediaCaptured(object? sender, MediaCapturedEventArgs e)
+        {
+            // Acessando o Dispatcher via MainThread
+            if (MainThread.IsMainThread)
+            {
+                MyImage.Source = ImageSource.FromStream(() => e.Media);
+                return;
+            }
+
+            // Caso o código não esteja no thread principal, usamos o MainThread para atualizar
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                MyImage.Source = ImageSource.FromStream(() => e.Media);
+            });
+        }
+
+        public async Task CaptureImageAsync(CancellationToken cancellationToken)
+        {
+            await MyCamera.CaptureImage(cancellationToken);
+        }
+    }
+}
---
+++ MauiCamera/MauiCamera/MauiCameraViewSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Core;
+using Microsoft.Maui.ApplicationModel;
+
+namespace MauiCameraViewSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private CameraUses cameraUses;
+
+        public MainPage(ICameraProvider cameraProvider)
+        {
+            InitializeComponent();
+
+            // Instanciando a classe CameraUses
+            cameraUses = new CameraUses(cameraProvider, MyCamera, MyImage);
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Inicializa a câmera ao aparecer
+            await cameraUses.InitializeCameraAsync();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            // Limpa os eventos e a câmera ao desaparecer
+            cameraUses.Dispose();
+        }
+
+        private async void GravarCamera(object sender, EventArgs e)
+        {
+            await cameraUses.CaptureImageAsync(CancellationToken.None);
+        }
+    }
+}
